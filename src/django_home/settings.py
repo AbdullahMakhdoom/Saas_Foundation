2a5a5db869b2062c98eb347bc98ae375eb61794c
Abdullah Makhdoom <abdullahmakhdoom1998@gmail.com>
2024-07-27 20:58:02 +0500

    Prepare for Railway.

--- src/django_home/settings.py
+++ src/django_home/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-%xetcs)em5u$(8f^871@kvvz3*flafw0j(h_+e7qn!7z)w^+%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app", # https://saas.prod.railway.app
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
--- src/django_home/views.py
+++ src/django_home/views.py
@@ -3,14 +3,23 @@ from django.shortcuts import render
 
 from visits.models import PageVisit 
 
-def home_page_view(request, *args, **kwargs):
+
+def about_view(request, *args, **kwargs):
+    return home_view(request, *args, **kwargs)
+
+def home_view(request, *args, **kwargs):
+
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() / qs.count()) * 100
+    except:
+        percent = 0
 
     page_title = {
         "page_title": "Hello World Title!",
         "page_visit_count": page_qs.count(),
-        "percent_vist_count": (page_qs.count() / qs.count()) * 100,
+        "percent_vist_count": percent,
         "total_visit_count": qs.count(),
     }
     PageVisit.objects.create(path=request.path)
