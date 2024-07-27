d64f4080b93fa709ec30de2904af79ea291172f9
Abdullah Makhdoom <abdullahmakhdoom1998@gmail.com>
2024-07-27 19:53:14 +0500

    Remove ignored files

--- src/django_home/views.py
+++ src/django_home/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from visits.models import PageVisit 
+
 def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+
     page_title = {
-        "page_title": "Hello World Title!"
+        "page_title": "Hello World Title!",
+        "page_visit_count": page_qs.count(),
+        "percent_vist_count": (page_qs.count() / qs.count()) * 100,
+        "total_visit_count": qs.count(),
     }
+    PageVisit.objects.create(path=request.path)
     return render(request, "home.html", page_title)
