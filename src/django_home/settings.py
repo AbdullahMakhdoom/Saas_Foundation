a298eb7810e172d8dc7500f101fd6d30a68a582c
Abdullah Makhdoom <abdullahmakhdoom1998@gmail.com>
2024-07-28 23:07:45 +0500

    chore: Add static files configuration for Django project

--- src/django_home/settings.py
+++ src/django_home/settings.py
@@ -139,6 +139,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILE_VENDOR_DIR = STATICFILES_BASE_DIR / "vendor"
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,]
+
+# output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR / "local_cdn"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
