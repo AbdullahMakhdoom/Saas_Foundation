22e33325a5885c5fab7e7c84ce407a25056eb6d4
Abdullah Makhdoom <abdullahmakhdoom1998@gmail.com>
2024-08-04 04:08:21 +0500

    chore: improve django allauth UI.

--- src/django_home/urls.py
+++ src/django_home/urls.py
@@ -21,7 +21,7 @@ from auth import views as auth_views
 from .views import home_view, about_view
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name="home"),
     path("hello-world/", home_view),
     path("about/", about_view),
     path('admin/', admin.site.urls),
